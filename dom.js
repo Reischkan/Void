971bf4425f84cd49c0990d2cb08629e73ae3d38f
Reischkan <andres.lowliet@gmail.com>
2020-10-12 22:31:12 -0500

    error

--- dom.js
+++ dom.js
@@ -1,22 +1,27 @@
 
 valor = document.getElementById("inputEmail4").value;
 if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor)) ) {
-    alert('[ERROR] El campo debe tener un valor de...');
+    alert('[ERROR]');
   return false;
 }
 valor = document.getElementById("inputPassword4").value;
 if( isNaN(valor) ) {
+    alert('[ERROR]');
   return false;
 }
 valor = document.getElementById("inputZip").value;
 if( isNaN(valor) ) {
+    alert('[ERROR]');
   return false;
 }
 valor = document.getElementById("inputCity").value;
 if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-  return false;
+    alert('[ERROR]');
+    return false;
+  
 }
 elemento = document.getElementById("gridCheck");
 if( !elemento.checked ) {
+    alert('[ERROR]');
   return false;
 }
